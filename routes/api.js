05fdb0f584fb91508bde77765b640e95171a58bf
Clifford Morin <cemorin21@gmail.com>
2021-06-02 11:35:54 -0400

    formated appi.js

--- routes/api.js
+++ routes/api.js
@@ -2,42 +2,42 @@ const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
 router.get("/api/workouts", (req, res) => {
-Workout.aggregate( [
-  {
-    $addFields: {
-      totalDuration: { $sum: "$exercises.duration" } 
-    }
-  },
-])
-.then(dbWorkout => {
-  res.json(dbWorkout);
-})
-.catch(err => {
-  res.status(400).json(err);
-});
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
+    },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 router.get("/api/workouts/range", (req, res) => {
-  Workout.aggregate( [
+  Workout.aggregate([
     {
       $addFields: {
-        totalDuration: { $sum: "$exercises.duration" } 
-      }
+        totalDuration: { $sum: "$exercises.duration" },
+      },
     },
-  ]).limit(7).sort({ day: 1})
-  
-  .then(dbWorkout => {
-    res.json(dbWorkout);
-  })
-  .catch(err => {
-    res.status(400).json(err);
-  });
-  });
+  ])
+    .limit(7)
+    .sort({ day: 1 })
 
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 //Put
 
 //Post
-
 
 module.exports = router;
